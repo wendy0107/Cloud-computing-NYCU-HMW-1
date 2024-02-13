1d9ce91af9f4ede8870385545e66b0e477a7392b
Wen Li <79928462+wendy0107@users.noreply.github.com>
2024-02-13 15:36:10 +0800

    Delete 112550816_Homework_1 directory

--- 112550816_Homework_1/install-minio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://dl.min.io/server/minio/release/linux-amd64/archive/minio_20231007150738.0.0_amd64.deb -O minio.deb
-sudo dpkg -i minio.deb
-
-mkdir ~/minio
-minio server ~/minio --console-address :8080
